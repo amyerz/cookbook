cfb2e299b1f8d1a8de6096249c4f02c37bdca910
Amy Erz <wingmerz@gmail.com>
2013-06-12 22:34:37 -0700

    added twitter bootstrap

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cookbook::Application.config.secret_token = '884cf28b12d26f077f674b288680372cdbe7f7a25de5150faaa728337675725afe033297a80f2b36f83077d4546c2f7869e90dd62f49817dcc795e0a7248e12d'
